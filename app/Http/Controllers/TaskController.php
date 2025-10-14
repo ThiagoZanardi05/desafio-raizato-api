b99c0bde3040fc10fed8f3eb0c31412f6d3ef880
Thiago Zanardi <thiagozanardi05@gmail.com>
2025-10-14 18:15:25 -0300

    feat: Implement task deletion endpoint

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -61,8 +61,10 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->json(null, 204);
     }
 }
