73773c6bced3cbc92ba5980587072d175ae2e274
Thiago Zanardi <thiagozanardi05@gmail.com>
2025-10-14 17:40:10 -0300

    feat: Implement task showing endpoint

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -37,9 +37,9 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return $task;
     }
 
     /**
